f13f5711b8868e73675d811ec1c61f84b7dd5ca1
Junglajaime <jaimerodriguezbenitez@iestorredelrey.es>
2024-01-23 16:47:01 +0100

    commit

---
+++ script.js
@@ -0,0 +1,140 @@
+var lang = {
+    'es': {
+        'entrantes': 'Entrantes Fríos',
+        'raciones': 'Raciones',
+        'platos': 'Platos Principales',
+        'postres': 'Postres',
+        'contacto': 'Contacto',
+        'adress': 'Dirección',
+        'phone number': 'Teléfono',
+        'language': 'Idioma',
+        'pedir_ahora': 'Pedir ahora',
+        'patatas_alioli': 'Patatas Alioli',
+        'patatas_alioli_desc': 'Deliciosas patatas acompañadas de una suave salsa alioli.',
+        'jamon_iberico': 'Jamón Ibérico',
+        'jamon_iberico_desc': 'Exquisito jamón ibérico de bellota, cortado en finas lonchas.',
+        'foie_pato_trufado': 'Foie de Pato Trufado',
+        'foie_pato_trufado_desc': 'Foie gras de pato con un toque de trufa, acompañado de pan tostado.',
+        'tataki_atun_wasabi': 'Tataki de Atún con Wasabi',
+        'tataki_atun_wasabi_desc': 'Atún fresco ligeramente sellado y servido con wasabi y salsa de soja.',
+        'ceviche': 'Ceviche',
+        'ceviche_desc': 'Pescado marinado en limón, cilantro y especias, servido con aguacate y maíz.',
+        'choco_frito': 'Choco Frito',
+        'choco_frito_desc': 'Deliciosos calamares chocos fritos, acompañados de salsa alioli.',
+        'alitas_barbacoa': 'Alitas a la Barbacoa',
+        'alitas_barbacoa_desc': 'Alitas de pollo bañadas en salsa barbacoa, acompañadas de bastones de apio y zanahoria.',
+        'puntillitas': 'Puntillitas',
+        'puntillitas_desc': 'Puntillitas de calamar fritas con un toque de limón, ideales para picar.',
+        'croquetas_jamon': 'Croquetas de Jamón',
+        'croquetas_jamon_desc': 'Suaves croquetas caseras rellenas de jamón ibérico, crujientes por fuera y cremosas por dentro.',
+        'platos': 'Platos Principales',
+        'postres': 'Postres',
+        'tomahawk_nombre': 'Tomahawk de Ternera Rubia Gallega al Carbón',
+        'tomahawk_desc': 'Jugoso tomahawk de ternera rubia gallega cocido a la perfección al carbón, acompañado de guarniciones frescas y salsas exquisitas.',
+        'pulpo_a_la_brasa_nombre': 'Pulpo a la Brasa',
+        'pulpo_a_la_brasa_desc': 'Pulpo tierno a la brasa con un toque ahumado, servido con patatas asadas y alioli casero.',
+        'rissotto_nombre': 'Risotto de Setas y Trufas',
+        'rissotto_desc': 'Risotto cremoso preparado con una mezcla de setas silvestres y trufas, cocido a la perfección y decorado con virutas de parmesano.',
+        'bacalao_nombre': 'Bacalao a la Vizcaína',
+        'bacalao_desc': 'Jugoso bacalao preparado a la vizcaína con tomate, pimientos y especias, acompañado de puré de patatas y espárragos.',
+        'tarta_queso': 'Tarta de Queso',
+        'tarta_queso_desc': 'Deliciosa tarta de queso horneada a la perfección, adornada con frutas frescas y coulis de frambuesa.',
+        'coulant_praline': 'Coulant de Praliné',
+        'coulant_praline_desc': 'Irresistible coulant de chocolate con corazón de praliné, acompañado de helado de vainilla y salsa de chocolate.',
+        'torrija_helado': 'Torrija con Helado de Vainilla',
+        'torrija_helado_desc': 'Clásica torrija bañada en leche y canela, servida con helado de vainilla y almendras tostadas.',
+        'muerte_chocolate': 'Muerte por Chocolate',
+        'muerte_chocolate_desc': 'Una experiencia indulgente con tres tipos de chocolate, helado de chocolate negro y crujientes de cacao.',
+        'sobre_nosotros_titulo': 'Sobre Nosotros',
+        'sobre_nosotros_contenido': 'En nuestro restaurante moderno y vanguardista, nos dedicamos a proporcionar una experiencia culinaria excepcional mediante sabores innovadores y creativos. Nos esforzamos por cautivar a nuestros clientes con ingredientes frescos y de alta calidad, ofreciendo un ambiente acogedor y sofisticado. Como líderes gastronómicos, nuestra visión es ser un destino preferido a nivel mundial, expandiendo nuestra presencia con platos distintivos y un compromiso con la excelencia. Nuestros valores incluyen la pasión por la excelencia, la creatividad, la hospitalidad, la sostenibilidad y la integridad, formando la base de nuestro éxito y reputación.',
+        'direccion_titulo': 'Dirección',
+        'encuentranos_titulo': 'Encuéntranos',
+        'redes_sociales_titulo': 'Redes Sociales',
+        'derechos_reservados': '©2023 · Todos los derechos reservados.',
+        'politica_privacidad': 'Política de Privacidad',
+        'politica_cookies': 'Política de Cookies',
+        'aviso_legal': 'Aviso Legal'
+    },
+    'en': {
+        'entrantes': 'Cold Starters',
+        'raciones': 'Appetizers',
+        'platos': 'Main Courses',
+        'postres': 'Desserts',
+        'contacto': 'Contact',
+        'adress': 'Adress',
+        'phone number': 'Phone number',
+        'language': 'Language',
+        'pedir_ahora': 'Order now',
+        'patatas_alioli': 'Patatas Alioli',
+        'patatas_alioli_desc': 'Delicious potatoes accompanied by a smooth alioli sauce.',
+        'jamon_iberico': 'Iberian Ham',
+        'jamon_iberico_desc': 'Exquisite Iberian acorn-fed ham, thinly sliced.',
+        'foie_pato_trufado': 'Truffled Duck Foie Gras',
+        'foie_pato_trufado_desc': 'Duck foie gras with a touch of truffle, accompanied by toasted bread.',
+        'tataki_atun_wasabi': 'Tuna Tataki with Wasabi',
+        'tataki_atun_wasabi_desc': 'Fresh tuna lightly seared and served with wasabi and soy sauce.',
+        'ceviche': 'Ceviche',
+        'ceviche_desc': 'Fish marinated in lemon, cilantro, and spices, served with avocado and corn.',
+        'choco_frito': 'Fried Squid',
+        'choco_frito_desc': 'Delicious fried squid, accompanied by alioli sauce.',
+        'alitas_barbacoa': 'BBQ Chicken Wings',
+        'alitas_barbacoa_desc': 'Chicken wings coated in barbecue sauce, served with celery and carrot sticks.',
+        'puntillitas': 'Baby Squid Bites',
+        'puntillitas_desc': 'Fried baby squid bites with a hint of lemon, perfect for snacking.',
+        'croquetas_jamon': 'Ham Croquettes',
+        'croquetas_jamon_desc': 'Soft homemade croquettes filled with Iberian ham, crispy on the outside and creamy on the inside.','platos': 'Main Courses',
+        'postres': 'Desserts',
+        'tomahawk_nombre': 'Char-Grilled Galician Blond Veal Tomahawk',
+        'tomahawk_desc': 'Juicy char-grilled Galician blond veal tomahawk cooked to perfection, accompanied by fresh garnishes and exquisite sauces.',
+        'pulpo_a_la_brasa_nombre': 'Grilled Octopus',
+        'pulpo_a_la_brasa_desc': 'Tender grilled octopus with a smoky touch, served with roasted potatoes and homemade alioli sauce.',
+        'rissotto_nombre': 'Wild Mushroom and Truffle Risotto',
+        'rissotto_desc': 'Creamy risotto prepared with a mix of wild mushrooms and truffles, cooked to perfection, and garnished with parmesan shavings.',
+        'bacalao_nombre': 'Basque-Style Cod',
+        'bacalao_desc': 'Juicy cod prepared Basque-style with tomatoes, bell peppers, and spices, served with mashed potatoes and asparagus.',
+        'tarta_queso': 'Cheesecake',
+        'tarta_queso_desc': 'Delicious cheesecake baked to perfection, garnished with fresh fruits and raspberry coulis.',
+        'coulant_praline': 'Praliné Coulant',
+        'coulant_praline_desc': 'Irresistible chocolate coulant with a praliné heart, served with vanilla ice cream and chocolate sauce.',
+        'torrija_helado': 'French Toast with Vanilla Ice Cream',
+        'torrija_helado_desc': 'Classic French toast soaked in milk and cinnamon, served with vanilla ice cream and toasted almonds.',
+        'muerte_chocolate': 'Death by Chocolate',
+        'muerte_chocolate_desc': 'An indulgent experience with three types of chocolate, dark chocolate ice cream, and cocoa crisps.' ,
+        'sobre_nosotros_titulo': 'About Us',
+        'sobre_nosotros_contenido': 'In our modern and avant-garde restaurant, we are dedicated to providing an exceptional culinary experience through innovative and creative flavors. We strive to captivate our customers with fresh and high-quality ingredients, offering a cozy and sophisticated atmosphere. As gastronomic leaders, our vision is to be a preferred global destination, expanding our presence with distinctive dishes and a commitment to excellence. Our values include passion for excellence, creativity, hospitality, sustainability, and integrity, forming the foundation of our success and reputation.',
+        'direccion_titulo': 'Address',
+        'encuentranos_titulo': 'Find Us',
+        'redes_sociales_titulo': 'Social Media',
+        'derechos_reservados': '©2023 · All rights reserved.',
+        'politica_privacidad': 'Privacy Policy',
+        'politica_cookies': 'Cookie Policy',
+        'aviso_legal': 'Legal Notice'
+    }
+};
+
+
+
+
+function changeLanguage(language) {
+    var elements = document.querySelectorAll('[data-translate]');
+    elements.forEach(function (element) {
+        var key = element.getAttribute('data-translate');
+        element.textContent = lang[language][key];
+    });
+
+    // Actualiza las imágenes de las banderas
+    var flagES = document.querySelector('.flag-es');
+    var flagEN = document.querySelector('.flag-en');
+    flagES.style.fontWeight = (language === 'es') ? 'bold' : 'normal';
+    flagEN.style.fontWeight = (language === 'en') ? 'bold' : 'normal';
+
+    // Mantiene activa la opción de idioma seleccionada
+    document.getElementById(language).classList.add('active');
+}
+
+
+
+
+
+
+
